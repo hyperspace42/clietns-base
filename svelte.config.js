667efba9a29ae31610dfecb5eda7fe9df1e4bd4e
hyperspace42 <kivs00@list.ru>
2024-09-04 12:18:13 +0300

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-auto';
+import path from 'path';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		alias: {
+			'@components': path.resolve('./src/lib/components'),
+			'@lib': path.resolve('./src/lib'),
+			'@routes': path.resolve('./src/lib/routes')
+		}
+	}
+};
+
+export default config;
